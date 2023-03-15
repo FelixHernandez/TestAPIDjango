d3fb28176b6b898d44b011d935d03e8a7665316b
Felix Hernandez <felixfhdez15@gmail.com>
2023-03-15 00:29:54 -0600

    fix endpoints

--- src/employees/views.py
+++ src/employees/views.py
@@ -22,7 +22,7 @@ class EmployeesView(APIView):
         serializer = EmployeeSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
    
@@ -31,26 +31,26 @@ class EmployeesView(APIView):
         if id:
             employee = Employee.objects.get(pk=id)
             serializer = EmployeeSerializer(employee)
-            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_200_OK)
  
         employees = Employee.objects.all()
         serializer = EmployeeSerializer(employees, many=True)
-        return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)  
+        return Response(serializer.data, status=status.HTTP_200_OK)  
  
     def put(self, request, id=None):
         employee = Employee.objects.get(pk=id)
         serializer = EmployeeSerializer(employee, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
-            return Response({"status": "success", "data": serializer.data})
+            return Response(serializer.data)
         else:
             return Response({"status": "error", "data": serializer.errors})
  
  
     def delete(self, request, id=None):
-        student = get_object_or_404(Employee, id=id)
-        student.is_deleted = True
-        return Response({"status": "success", "data": "student Deleted"})
+        employee = get_object_or_404(Employee, id=id)
+        employee.is_deleted = True
+        return Response({"status": "success", "data": "employee Deleted"})
    
  
 class BeneficiariesView(APIView):
@@ -59,17 +59,17 @@ class BeneficiariesView(APIView):
         if id:
             beneficiary = Beneficiary.objects.get(pk=id)
             serializer = BeneficiarySerializer(user)
-            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+            return Response( serializer.data, status=status.HTTP_200_OK)
  
         beneficiary = Beneficiary.objects.all()
         serializer = BeneficiarySerializer(beneficiary, many=True)
-        return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_200_OK)
    
     def post(self, request):
         serializer = BeneficiarySerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
    
@@ -79,6 +79,6 @@ class BeneficiariesView(APIView):
         serializer = BeneficiarySerializer(beneficiary, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
-            return Response({"status": "success", "data": serializer.data})
+            return Response(serializer.data)
         else:
             return Response({"status": "error", "data": serializer.errors})
