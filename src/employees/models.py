caf9db489693298b624ce48c74941b42ff0b87bb
Felix Hernandez <felixfhdez15@gmail.com>
2023-03-15 01:06:39 -0600

    relations

--- src/employees/models.py
+++ src/employees/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+class Beneficiary(models.Model):
+    Name = models.CharField(max_length=255)
+    Relationship = models.CharField(max_length=255)
+    BirthDate = models.DateTimeField()
+    Gender = models.CharField(max_length=255)
+    IsDeleted = models.BooleanField(default=False)
+
 class Employee(models.Model):
     Photo = models.CharField(max_length=255)
     Name = models.CharField(max_length=255)
@@ -8,11 +15,5 @@ class Employee(models.Model):
     Status = models.CharField(max_length=255)
     HireDate = models.DateTimeField()
     IsDeleted = models.BooleanField(default=False)
+    Beneficiary = models.ForeignKey(Beneficiary, on_delete=models.CASCADE, related_name='employee')
 
-class Beneficiary(models.Model):
-    Employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='beneficiaries')
-    Name = models.CharField(max_length=255)
-    Relationship = models.CharField(max_length=255)
-    BirthDate = models.DateTimeField()
-    Gender = models.CharField(max_length=255)
-    IsDeleted = models.BooleanField(default=False)
