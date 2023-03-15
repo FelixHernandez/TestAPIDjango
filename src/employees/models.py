59376cd06781f8d6c8001a6be782dcabfd3f7df1
Felix Hernandez <felixfhdez15@gmail.com>
2023-03-15 00:56:42 -0600

    migrate

--- src/employees/models.py
+++ src/employees/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
 class Employee(models.Model):
-    photo = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    job_position = models.CharField(max_length=255)
-    wage = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.CharField(max_length=255)
-    hire_date = models.DateTimeField()
-    is_deleted = models.BooleanField(default=False)
+    Photo = models.CharField(max_length=255)
+    Name = models.CharField(max_length=255)
+    JobPosition = models.CharField(max_length=255)
+    Wage = models.DecimalField(max_digits=10, decimal_places=2)
+    Status = models.CharField(max_length=255)
+    HireDate = models.DateTimeField()
+    IsDeleted = models.BooleanField(default=False)
 
 class Beneficiary(models.Model):
-    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='beneficiaries')
-    name = models.CharField(max_length=255)
-    relationship = models.CharField(max_length=255)
-    birth_date = models.DateTimeField()
-    gender = models.CharField(max_length=255)
-    is_deleted = models.BooleanField(default=False)
+    Employee = models.ForeignKey(Employee, on_delete=models.CASCADE, related_name='beneficiaries')
+    Name = models.CharField(max_length=255)
+    Relationship = models.CharField(max_length=255)
+    BirthDate = models.DateTimeField()
+    Gender = models.CharField(max_length=255)
+    IsDeleted = models.BooleanField(default=False)
